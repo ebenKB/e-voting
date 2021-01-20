237bf3c83613b646a49939fce89b69b15015887e
Ebenezer K.B Adjei <ekb.adjei@yahoo.com>
2021-01-20 04:55:36 +0100

    add alt text to image tag

--- src/components/ImageHolder/ImageHolder.jsx
+++ src/components/ImageHolder/ImageHolder.jsx
@@ -23,7 +23,7 @@ const ImageHolder = ({image, lable="blue", name="", slogan="", team="", handleAc
             <div className={`${Styles[lable]} ${Styles.lable}`}>{team}</div>
             <div className={Styles.overlay}>
                 <div>
-                    <img src={Finger} />
+                    <img src={Finger} alt="image" />
                 </div>
                 <button onClick={handleVoteAction}>VOTE</button>
             </div>
